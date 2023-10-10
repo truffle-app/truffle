2590f03929bf373ab82447f3894f601cdde9fcd0
Mikko <mikko.e.suhonen@gmail.com>
2023-10-10 13:45:27 +0300

    Implemented postgres initialization

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,23 +1,23 @@
 import postgres from 'postgres'
 
-const devConfig = {
+const devConfig: {
+  user: string | undefined,
+  host: string | undefined,
+  database: string | undefined,
+  password: string | undefined,
+  port: number | undefined
+} = {
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT
+  port: Number(process.env.POSTGRES_PORT)
 }
 
 const prodConfig = {
   connectionString: process.env.DATABASE_URL
 }
 
-const sql = postgres('postgres://username:password@host:port/database', {
-  host                 : '',            // Postgres ip address[s] or domain name[s]
-  port                 : 5432,          // Postgres server port[s]
-  database             : '',            // Name of database to connect to
-  username             : '',            // Username of database user
-  password             : ''            // Password of database user
-})
+const sql = postgres('postgres://username:password@host:port/database', devConfig)
 
 export default sql
