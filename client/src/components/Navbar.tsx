d73dae340f6e90abf1e038ca34ba6cfe9be32008
Mikko <mikko.e.suhonen@gmail.com>
2023-10-17 12:35:40 +0300

    Added types and cleaned some things

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
+import { useState } from 'react'
 import React, { View, StyleSheet, Pressable } from 'react-native'
 import NavbarTab from './NavbarTab'
-import theme from '../theme'
 import { NavigateFunction } from 'react-router-native'
 
 const styles = StyleSheet.create({
@@ -9,21 +9,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end'
   },
-  tabItem: {
-    flexGrow: 1,
-    height: 60,
-    backgroundColor: theme.colors.navbarBackgroundColor,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
 })
 
 const Navbar = ({navigateTo}: {navigateTo: NavigateFunction}) => {
+  const [activeTab, setActiveTab] = useState('/feed')
+
+  const navigateToLocation = (location: string) => {
+    setActiveTab(location)
+    navigateTo(location)
+  }
+
   return (
     <View style={styles.container}>
-      <NavbarTab style={styles.tabItem} onPressFunction={() => navigateTo('/feed')} text={'Feed'}/>
-      <NavbarTab style={styles.tabItem} onPressFunction={() => navigateTo('/explore')} text={'Explore'}/>
-      <NavbarTab style={styles.tabItem} onPressFunction={() => navigateTo('/profile')} text={'Profile'}/>
+      <NavbarTab onPressFunction={() => navigateToLocation('/feed')} text={'Feed'} isActive={activeTab ==='feed'}/>
+      <NavbarTab onPressFunction={() => navigateToLocation('/explore')} text={'Explore'} isActive={activeTab ==='explore'}/>
+      <NavbarTab onPressFunction={() => navigateToLocation('/profile')} text={'Profile'} isActive={activeTab ==='profile'}/>
     </View>
   )
 }
--- client/src/components/NavbarTab.tsx
+++ client/src/components/NavbarTab.tsx
@@ -1,9 +1,34 @@
 import React, { View, StyleSheet, Pressable } from 'react-native'
 import BigText from './BigText'
+import theme from '../theme'
 
-const NavbarTab = ({style, onPressFunction, text}) => {
+const styles = StyleSheet.create({
+  tabItem: {
+    flexGrow: 1,
+    height: 60,
+    backgroundColor: theme.colors.navbarBackgroundColor,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  highlight: {
+    height: 0,
+    backgroundColor: '#00000'
+  }
+})
+
+const NavbarTab = ({
+  onPressFunction,
+  text,
+  isActive = false
+}: {
+  onPressFunction: any,
+  text: string,
+  isActive: boolean
+}) => {
+
   return (
-    <Pressable style={style} onPress={onPressFunction}>
+    <Pressable style={styles.tabItem} onPress={onPressFunction}>
+      <View style={styles.highlight}></View>
       <BigText>{text}</BigText>
     </Pressable>
   )
